83a2ecebe9cfcca8874f6645432b50992ef23eee
Og-Miles <emmanuelogbuji5@gmail.com>
2023-10-14 19:15:48 +0100

    screen-height

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className='max-w-7xl px-5 lg:px-10 mx-auto space-x-2 py-2'>
+    <div className='max-w-7xl px-5 lg:px-10 mx-auto space-x-2 py-2 overflow-hidden h-screen'>
       <Navbar />
       <Hero />
     </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,10 +39,10 @@ function Hero() {
         <p className='mt-3 text-slate-400'>Based Remotely</p>
         {/* call to action */}
         <div className='flex-col lg:flex-row justify-between mt-3 mx-auto'>
-          <button className='border border-black text-white bg-black px-5 py-3 min-w-[220px] lg:mr-2 mt-5'>
+          <button className='border border-black text-white bg-black px-5 py-3 min-w-[220px] lg:mr-2 mt-5 rounded'>
             Need a Web developer
           </button>
-          <button className='border border-black bg-white text-black px-5 py-3 min-w-[220px] lg:ml-2 mt-5'>
+          <button className='border border-black bg-white text-black px-5 py-3 min-w-[220px] lg:ml-2 mt-5 rounded'>
             Need a Web designer
           </button>
         </div>
